e010b3b9f020ddc4de00582d77d3effcd144ac63
manuelurruti <manuelurrutimarquez@gmail.com>
2023-04-17 05:38:32 -0300

    asdasd

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,78 +3,26 @@ import { animated } from 'react-spring';
 import { use3dEffect } from 'use-3d-effect';
 import Carousel from 'react-grid-carousel'
 import "./Card.css"
-import { Link } from 'react-router-dom';
 
 export const Card  = () => {
 
     return (
 
-              <Carousel cols={3} rows={1} gap={1} loop>
-            
-              <Carousel.Item>
-             <img width="95%" className='photo' src="../../image4.png" />
-                <h3 className='parraf'>Perfumall</h3>
-                <p className='parraf'>e-commerce of perfumes.</p>
-                <p className='parraf'>Fullstack</p>
-  
 
+            <Carousel cols={3} rows={1} gap={4} loop>
+              <Carousel.Item>
+                <img width="95%" src="https://picsum.photos/800/600?random=1" />
               </Carousel.Item>
               <Carousel.Item>
-            <img width="95%" className='photo' src="../../image2.png" />
-                <h3 className='parraf'>Dashboard of Perfumall</h3>
-               
+                <img width="95%" src="https://picsum.photos/800/600?random=2" />
               </Carousel.Item>
               <Carousel.Item>
-             <img width="95%" className='photo' src="../../api.png" />
-              <h3 className='parraf'>API of Perfumall.</h3>
-             
-
+                <img width="95%" src="https://picsum.photos/800/600?random=3" />
               </Carousel.Item>
               <Carousel.Item>
-              <img width="95%" className='photo' src="../../anime.png" />
-              <h3  className='parraf'>Anime!</h3>
-             
-
-              </Carousel.Item>
-            
-              <Carousel.Item>
-             <img width="95%" className='photo' src="../../image3.png" />
-              <h3 className='parraf'>Vegan proyect</h3>
-              <p className='parraf'>Full stack</p>
-              <p className='parraf'>In progress...</p>
-
-
-
-              </Carousel.Item>
-              <Carousel.Item>
-             <img width="95%" className='photo' src="../../image1.png" />
-              <h3 className='parraf'>League of legends app</h3>
-
-              </Carousel.Item>
-              <Carousel.Item>
-             <img width="95%" className='photo' src="../../image5.png" />
-                <h3 className='parraf'>Weather app</h3>
-               
-                
-              </Carousel.Item>
-              <Carousel.Item>
-              <img width="95%" className='photo' src="../../image6.png" />
-              <h3 className='parraf'>Films</h3>
-
-              </Carousel.Item>
-            
-            
-              <Carousel.Item>
-              <img width="95%" className='photo' src="../../rick.png" />
-              <h3 className='parraf'>Rick & Morty</h3>
-
-
+                {/* anything you want to show in the grid */}
               </Carousel.Item>
               {/* ... */}
-              
             </Carousel>
-         
           )
         }
-
-
