36e925cacae0f646f4941d76fb0e9a3d7ab1f3e7
manuelurruti <manuelurrutimarquez@gmail.com>
2023-04-17 05:18:47 -0300

    asd

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,12 +7,13 @@ import "./Card.css"
 export const Card  = () => {
 
     return (
-
+ <> 
+ <h1 className='d-flex justify-content-center p-3 parraf'>My proyects!</h1>
               <Carousel cols={3} rows={1} gap={1} loop>
             
               <Carousel.Item>
               <a href="https://perfumall-mv7u.onrender.com/" target='a_blank'><img width="95%" className='photo' src="../../image4.png" /></a>
-                <h3 className='parraf'><a href="https://perfumall-mv7u.onrender.com/" target='a_blank' className='letter'>Perfumall</a></h3>
+                <h3 className='parraf'>Perfumall</h3>
                 <p className='parraf'>e-commerce of perfumes.</p>
                 <p className='parraf'>Fullstack</p>
   
@@ -20,12 +21,12 @@ export const Card  = () => {
               </Carousel.Item>
               <Carousel.Item>
               <a href="https://dashboard-tmwc.onrender.com" target='a_blank'><img width="95%" className='photo' src="../../image2.png" /></a>
-                <h3 className='parraf'><a href="https://dashboard-tmwc.onrender.com" target='a_blank' className='letter'>Dashboard of Perfumall.</a></h3>
+                <h3 className='parraf'>Dashboard of Perfumall</h3>
                
               </Carousel.Item>
               <Carousel.Item>
-              <img width="95%" className='photo' src="../../api.png" />
-              <h3 className='parraf'><a href="https://github.com/manuelurruti/API-perfumall" target='a_blank' className='letter'>API of Perfumall.</a></h3>
+              <a href="https://github.com/manuelurruti/API-perfumall" target='a_blank' className='letter'> <img width="95%" className='photo' src="../../api.png" /></a>
+              <h3 className='parraf'>API of Perfumall.</h3>
              
 
               </Carousel.Item>
@@ -38,7 +39,7 @@ export const Card  = () => {
             
               <Carousel.Item>
               <a href="https://veganprxjxt.onrender.com/" target='a_blank'><img width="95%" className='photo' src="../../image3.png" /></a>
-              <h3 className='parraf'><a href="https://veganprxjxt.onrender.com/" target='a_blank' className='letter'>Vegan proyect</a></h3>
+              <h3 className='parraf'>Vegan proyect</h3>
               <p className='parraf'>Full stack</p>
               <p className='parraf'>In progress...</p>
 
@@ -47,32 +48,32 @@ export const Card  = () => {
               </Carousel.Item>
               <Carousel.Item>
               <a href="https://super-biscotti-2b2162.netlify.app/" target='a_blank'><img width="95%" className='photo' src="../../image1.png" /></a>
-              <h3 className='parraf'><a href="https://super-biscotti-2b2162.netlify.app/" target='a_blank' className='letter'>League of legends app</a></h3>
+              <h3 className='parraf'>League of legends app</h3>
 
               </Carousel.Item>
               <Carousel.Item>
               <a href="https://wheaterapp-efdp.onrender.com/" target='a_blank'><img width="95%" className='photo' src="../../image5.png" /></a>
-                <h3 className='parraf'><a href="https://wheaterapp-efdp.onrender.com/" target='a_blank' className='letter'>Weather app</a></h3>
+                <h3 className='parraf'>Weather app</h3>
                
                 
               </Carousel.Item>
               <Carousel.Item>
               <a href="https://films-1ct6.onrender.com/" target='a_blank'><img width="95%" className='photo' src="../../image6.png" /></a>
-              <h3 className='parraf'><a href="https://films-1ct6.onrender.com/" target='a_blank' className='letter'>Films</a></h3>
+              <h3 className='parraf'>Films</h3>
 
               </Carousel.Item>
             
             
               <Carousel.Item>
               <a href="https://rikandmortyx.onrender.com/" target='a_blank'><img width="95%" className='photo' src="../../rick.png" /></a>
-              <h3 className='parraf'><a href="https://rikandmortyx.onrender.com/" target='a_blank' className='letter'>Rick & Morty</a></h3>
+              <h3 className='parraf'>Rick & Morty</h3>
 
 
               </Carousel.Item>
               {/* ... */}
               
             </Carousel>
-     
+            </>
           )
         }
 
